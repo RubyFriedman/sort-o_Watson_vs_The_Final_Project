ba10ba133a2f42c17dd5d5b32c95cb87443f708c
rubyfriedman <rfriedman30@stuy.edu>
2022-01-10 15:54:21 -0500

    final code edits

--- Driver.java
+++ Driver.java
@@ -23,76 +23,60 @@ public class Driver{
     }
 
     public static void main(String[] args) {
-        ArrayList kaz = new ArrayList<Integer>();
+        ArrayList kaz = new ArrayList<Integer>(); //og array nums
         kaz.add(7);
         kaz.add(1);
         kaz.add(5);
         kaz.add(12);
         kaz.add(3);
 
-	ArrayList best = new ArrayList<Integer>();
+	ArrayList best = new ArrayList<Integer>(); // best case
 	for (int i = 0; i < 10; i++) {
 		best.add(i);
 	}
 
-	ArrayList worst = new ArrayList<Integer>();
-        for (int i = 10; i > 0; i--) {
+	ArrayList worst = new ArrayList<Integer>(); // worst case
+        for (int i = 9; i >= 0; i--) {
                 worst.add(i);
         }
 
-        //Bubbles
-        System.out.println("==========Bubble==========");
-        System.out.println( "\nArrayList kaz before sorting:\n" + kaz );
-        System.out.println( "\nArrayList kaz after sorting:\n");
-        System.out.println(Sorts.bubble(kaz));
+	ArrayList bigBad = new ArrayList<Integer>(); //length 1000
+        for (int i = 999; i >= 0; i--) {
+                bigBad.add(i);
+        }
+
+        ArrayList bigRand = populate( 1000, 1, 1000 ); //rand pop
+
+        System.out.println("" + kaz + ":");
+        Sorts.bubble(kaz);
+        Sorts.selection(kaz);
+        Sorts.insertion(kaz);
 
         ArrayList ketterdam = populate( 10, 1, 1000 );
-        System.out.println( "\nArrayList ketterdam before sorting:\n" + ketterdam );
-        System.out.println( "\nArrayList ketterdam after sorting:\n");
-	System.out.println( Sorts.bubble(ketterdam));
+        System.out.println( ketterdam + ":");
+	Sorts.bubble(ketterdam);
+        Sorts.selection(ketterdam);
+        Sorts.insertion(ketterdam);
 
-        System.out.println( "\nArrayList best before sorting:\n" + best );
-        System.out.println( "\nArrayList best after sorting:\n");
-        System.out.println( Sorts.bubble(best));
+        System.out.println("" + best + ":");
+        Sorts.bubble(best);
+        Sorts.selection(best);
+        Sorts.insertion(best);
 
-        System.out.println( "\nArrayList worst before sorting:\n" + worst );
-        System.out.println( "\nArrayList worst after sorting:\n");
-        System.out.println( Sorts.selection(worst));
+        System.out.println("" + worst + ":");
+        Sorts.bubble(worst);
+        Sorts.selection(worst);
+        Sorts.insertion(worst);
 
-        //Selection
-        System.out.println("==========Selection==========");
-        System.out.println( "\nArrayList kaz before sorting:\n" + kaz );
-        System.out.println( "\nArrayList kaz after sorting:\n");
-        System.out.println(Sorts.selection(kaz));
+        System.out.println("Array length 1K in descending order");
+        Sorts.bubble(bigBad);
+        Sorts.selection(bigBad);
+        Sorts.insertion(bigBad);
 
-        System.out.println( "\nArrayList ketterdam before sorting:\n" + ketterdam );
-        System.out.println( "\nArrayList ketterdam after sorting:\n");
-        System.out.println( Sorts.selection(ketterdam));
-
-        System.out.println( "\nArrayList best before sorting:\n" + best );
-        System.out.println( "\nArrayList best after sorting:\n");
-        System.out.println( Sorts.selection(best));
-
-        System.out.println( "\nArrayList worst before sorting:\n" + worst );
-        System.out.println( "\nArrayList worst after sorting:\n");
-        System.out.println( Sorts.selection(worst));
-        //Insertion
-        System.out.println("==========Insertion==========");
-        System.out.println( "\nArrayList kaz before sorting:\n" + kaz );
-        System.out.println( "\nArrayList kaz after sorting:\n");
-        System.out.println(Sorts.insertion(kaz));
-
-        System.out.println( "\nArrayList ketterdam before sorting:\n" + ketterdam );
-        System.out.println( "\nArrayList ketterdam after sorting:\n");
-        System.out.println( Sorts.insertion(ketterdam));
-
-        System.out.println( "\nArrayList best before sorting:\n" + best );
-        System.out.println( "\nArrayList best after sorting:\n");
-        System.out.println( Sorts.selection(best));
-
-        System.out.println( "\nArrayList worst before sorting:\n" + worst );
-        System.out.println( "\nArrayList worst after sorting:\n");
-        System.out.println( Sorts.selection(worst));
+        System.out.println("Array length 1k, random order");
+        Sorts.bubble(bigRand);
+        Sorts.selection(bigRand);
+        Sorts.insertion(bigRand);
     }
 
 
--- Sorts.java
+++ Sorts.java
@@ -1,83 +1,154 @@
 import java.util.ArrayList;
 
 public class Sorts {
-	public static ArrayList<Comparable> bubble( ArrayList<Comparable> data ) {
-		int swaps = 0;
-		ArrayList<Comparable> mutable = new ArrayList<Comparable>();
-		for (int i = 0; i<data.size();i++) {
-			mutable.add(data.get(i));
-		}
-		for( int passCtr = 1; passCtr < mutable.size(); passCtr++ ) {
-			//System.out.println( "commencing pass #" + passCtr + "..." ); 
-			//iterate thru first to next-to-last element, comparing to next
-			for( int i = 0; i < mutable.size()-1; i++ ) {
-				//if element at i > element at i+1, swap
-				if ( mutable.get(i).compareTo(mutable.get(i+1) ) > 0 ) {
-					mutable.set( i, mutable.set(i+1,mutable.get(i)) );
-					swaps += 1;
-				}
-				//System.out.println(data); //diag: show current state of list
-			}
-		}
-		System.out.println("Bubble Sort on the array took " + swaps + " swaps");
-		return mutable;
-	}
-	public static ArrayList<Comparable> selection( ArrayList<Comparable> data ) {
-    	//note: this version places greatest value at rightmost end,
-		//maxPos will point to position of SELECTION (greatest value)
-                ArrayList<Comparable> mutable = new ArrayList<Comparable>();
-                for (int i = 0; i<data.size();i++) {
-                        mutable.add(data.get(i));
-                }
-    	int swaps = 0;
-	int maxPos;
-    	for( int pass = mutable.size()-1; pass > 0; pass-- ) {
-      		//System.out.println( "\nbegin pass " + (data.size()-pass) );//diag
-      		maxPos = 0;
-      		for( int i = 1; i <= pass; i++ ) {
-        		//System.out.println( "maxPos: " + maxPos );//diag
-        		//System.out.println( data );//diag
-        		if ( mutable.get(i).compareTo( mutable.get(maxPos) ) > 0 ) {
-		      		maxPos = i;
-			  	}
-      		}
-		swaps += 1;
-      		mutable.set( maxPos, ( mutable.set( pass, mutable.get(maxPos) ) ) );
-		//System.out.println( "after swap: " +  data );//diag
-    	}
-                System.out.println("Selection Sort on the array took " + (swaps - 1) + " swaps");
-		return mutable;
-  	}//end selectionSort
 
 
-	public static ArrayList<Comparable> insertion ( ArrayList<Comparable> data ) {
-		int swaps = 0;
-                ArrayList<Comparable> mutable = new ArrayList<Comparable>();
-                for (int i = 0; i<data.size();i++) {
-                        mutable.add(data.get(i));
-                }
-		for( int partition = 1; partition < mutable.size(); partition++ ) {
-			//partition marks first item in unsorted region
-			//diag:
-			//System.out.println( "\npartition: " + partition + "\tdataset:");
-			//System.out.println( data );
-			//traverse sorted region from right to left
-			for( int i = partition; i > 0; i-- ) {
-				// "walk" the current item to where it belongs
-				// by swapping adjacent items
-				if ( mutable.get(i).compareTo( mutable.get(i-1) ) < 0 ) {
-					//diag:
-					//System.out.println( "swap indices "+(i-1)+" & "+i+"..." );
-					swaps += 1;
-					mutable.set( i, mutable.set( i-1, mutable.get(i) ) );
-			  	}
-			  	else {
-					break;
-				}
-		  	}
-		}
-                System.out.println("Insertion Sort on the array took " + swaps + " swaps");
-		return mutable;
-	}//end insertion
+  // VOID version of bubbleSort
+  // Rearranges elements of input ArrayList
+  // postcondition: data's elements sorted in ascending order
+  public static void bubbleSortV( ArrayList<Comparable> data )
+  {
+    int swap = 0;
+    int passes = 0;
+    int comp = 0;
+    for( int passCtr = 1; passCtr < data.size(); passCtr++ ) {
+//      System.out.println( "commencing pass #" + passCtr + "..." );
+
+      //iterate thru first to next-to-last element, comparing to next
+      for( int i = 0; i < data.size()-1; i++ ) {
+         comp += 1;
+        //if element at i > element at i+1, swap
+        if ( data.get(i).compareTo(data.get(i+1) ) > 0 ){
+          data.set( i, data.set(i+1,data.get(i)) );
+	  swap += 1;
+         }
+        //System.out.println(data); //diag: show current state of list
+      }
+      passes = passCtr;
+    }
+    System.out.print("In a data set of size " + data.size() + " bubble sort took " );
+    System.out.print( swap);
+    System.out.println(" swaps, " + passes + " passes and " + comp + " comparisons");
+  }
+
+
+  // ArrayList-returning bubbleSort
+  // postcondition: order of input ArrayList's elements unchanged
+  //                Returns sorted copy of input ArrayList.
+  public static ArrayList<Comparable> bubble( ArrayList<Comparable> input )
+  {
+    //declare and initialize empty ArrayList for copying
+    ArrayList<Comparable> data = new ArrayList<Comparable>();
+
+    //copy input ArrayList into working ArrayList
+    for( Comparable o : input )
+      data.add( o );
+
+    //sort working ArrayList
+    bubbleSortV( data );
+
+    return data;
+  }
+
+
+
+public static void selectionSortV( ArrayList<Comparable> data )
+  {
+    //note: this version places greatest value at rightmost end,
+
+    //maxPos will point to position of SELECTION (greatest value)
+    int maxPos;
+    int swap = 0;
+    int passes = 0;
+    int comp = 0;
+    for( int pass = data.size()-1; pass > 0; pass-- ) {
+      passes += 1;
+//      System.out.println( "\nbegin pass " + (data.size()-pass) );//diag
+      maxPos = 0;
+      for( int i = 1; i <= pass; i++ ) {
+    //    System.out.println( "maxPos: " + maxPos );//diag
+  //      System.out.println( data );//diag
+        comp += 1;
+        if ( data.get(i).compareTo( data.get(maxPos) ) > 0 ) {
+          maxPos = i;
+          swap += 1;}
+      }
+      data.set( maxPos, ( data.set( pass, data.get(maxPos) ) ) );
+      //System.out.println( "after swap: " +  data );//diag
+    }
+    System.out.print("In a data set of size " + data.size() + " selection sort took " );
+    System.out.print( swap);
+    System.out.println(" swaps, " + passes + " passes and " + comp + " comparisons");
+  }//end selectionSort
+  
+
+  // ArrayList-returning selectionSort
+  // postcondition: order of input ArrayList's elements unchanged
+  //                Returns sorted copy of input ArrayList.
+  public static ArrayList<Comparable> selection( ArrayList<Comparable> input )
+  {
+    //declare and initialize empty ArrayList for copying
+    ArrayList<Comparable> data = new ArrayList<Comparable>();
+
+    //copy input ArrayList into working ArrayList
+    for( Comparable o : input )
+      data.add( o );
+
+    //sort working ArrayList
+    selectionSortV( data );
+
+    return data;
+  }//end selectionSort
+
+
+ public static void insertionSortV( ArrayList<Comparable> data )
+  {
+    int swap = 0;
+    int passes = 0;
+    int comp = 0;
+    for( int partition = 1; partition < data.size(); partition++ ) {
+      //partition marks first item in unsorted region
+      passes = partition;
+      //diag:
+     // System.out.println( "\npartition: " + partition + "\tdataset:");
+      //System.out.println( data ); 
+
+      //traverse sorted region from right to left
+      for( int i = partition; i > 0; i-- ) {
+         comp += 1;
+        // "walk" the current item to where it belongs
+        // by swapping adjacent items
+        if ( data.get(i).compareTo( data.get(i-1) ) < 0 ) {
+          //diag:
+        //  System.out.println( "swap indices "+(i-1)+" & "+i+"..." );
+          data.set( i, data.set( i-1, data.get(i) ) );
+          swap += 1;
+        }
+        else {
+          break;
+         }
+      }
+    }
+    System.out.print("In a data set of size " + data.size() + " insertion sort took " );
+    System.out.print( swap);
+    System.out.println(" swaps, " + passes + " passes and " + comp + " comparisons");
+  }//end insertionSortV
+
+  public static ArrayList<Comparable>
+    insertion( ArrayList<Comparable> input )
+  {
+    //declare and initialize empty ArrayList for copying
+    ArrayList<Comparable> data = new ArrayList<Comparable>();
+
+    //copy input ArrayList into working ArrayList
+    for( Comparable o : input )
+      data.add( o );
+
+    //sort working ArrayList
+    insertionSortV( data );
+
+    //return working ArrayList
+    return data;
+  }//end insertionSort
+
 }
-
